cb1ecc78a6f9170012a6785d55555f7cf0c97354
atLYST <101388967+gabury1@users.noreply.github.com>
2022-03-19 01:35:48 +0900

    TCP 소켓을 써서 게임을 만들어봤다.
    다중 서버로 만들었음.
    클라이언트가 보는 화면은 모두 같음.
    클라이언트들은 방을 만들고, 들어갈 수 있음.(각 유저마나 하나씩 생성 가능)
    모든 방 안의 인원이 래디를 누르고 방장이 시작버튼을 누르면 시작.

---
+++ ColoringGame Ver.2/ColoringGame Ver.2/MainForm.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace ColoringGame_Ver._2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnRoom = new System.Windows.Forms.Button();
+            this.btnClient = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(386, 13);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(402, 425);
+            this.txtLog.TabIndex = 0;
+            // 
+            // btnRoom
+            // 
+            this.btnRoom.Location = new System.Drawing.Point(12, 112);
+            this.btnRoom.Name = "btnRoom";
+            this.btnRoom.Size = new System.Drawing.Size(163, 94);
+            this.btnRoom.TabIndex = 1;
+            this.btnRoom.Text = "방 개수";
+            this.btnRoom.UseVisualStyleBackColor = true;
+            // 
+            // btnClient
+            // 
+            this.btnClient.Location = new System.Drawing.Point(12, 12);
+            this.btnClient.Name = "btnClient";
+            this.btnClient.Size = new System.Drawing.Size(163, 94);
+            this.btnClient.TabIndex = 2;
+            this.btnClient.Text = "클라 수";
+            this.btnClient.UseVisualStyleBackColor = true;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(13, 413);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(283, 25);
+            this.txtSend.TabIndex = 3;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(303, 413);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "보내기";
+            this.btnSend.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 395);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "공지";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.btnClient);
+            this.Controls.Add(this.btnRoom);
+            this.Controls.Add(this.txtLog);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnRoom;
+        private System.Windows.Forms.Button btnClient;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ColoringGame Ver.2/ColoringGame Ver.2/MainForm.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+using Server;
+
+namespace ColoringGame_Ver._2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            listener = new Listener(2014);
+            clients = new List<Client>();
+            rooms = new List<Room>();
+            
+            tmrGReceiver = new System.Windows.Forms.Timer();
+            tmrGReceiver.Interval = 100;
+            tmrGReceiver.Tick += gReceive;
+            tmrGReceiver.Start();
+
+            LogView("서버 시작");
+            LogView("게스트 수신 타이머 시작");
+
+        }
+
+        Listener listener;
+        public List<Client> clients;
+        public List<Room> rooms; 
+        public int RoomCount = 0; // 방 번호를 매겨줌.
+
+        System.Windows.Forms.Timer tmrGReceiver;
+
+        //여기에 0페이즈 수신도 있음.
+        void gReceive(object sender, EventArgs e)
+        {
+
+            if (listener.WaitingCount > 0)
+            {
+                Guest guest = listener.GetGuest();
+
+                Thread.Sleep(100);
+                Client c = new Client(guest);
+                Thread.Sleep(100);
+
+                void Receive0(object s, EventArgs ev)
+                {
+
+                    System.Windows.Forms.Timer t = s as System.Windows.Forms.Timer;
+
+                    if (c.phase != 0) { return; }
+
+                    if (guest.Connected)
+                    {
+                        string msg = guest.GetMsg();
+
+                        if (msg != "")
+                        {
+                            if (c.phase == 0)
+                            {
+
+                                string tag = msg.Substring(0, 6); string content = msg.Substring(6);
+
+                                //방들의 정보 신호를 받았을 때
+                                if (tag == "#rfsh ")
+                                {
+                                    string temp = "0#rfsh ";
+                                    
+                                    for(int i = 0; i < rooms.Count; i++)
+                                    {
+                                        temp += rooms[i].RoomInfo();
+                                        if (i != rooms.Count - 1) temp += "&";
+
+                                    }
+
+                                   guest.SendMsg(temp);
+
+                                }
+                                else if(tag == "#name ")
+                                {
+                                    c.name = content;
+                                    LogView(c.name + "님의 참가");
+
+                                    Broadcast("0#allc " + c.name + "님이 게임에 접속하셨습니다.");
+                                }
+                                //방 만들기 신호를 받았을 때
+                                else if (tag == "#crte ")
+                                {
+                                    Room r = new Room(content, RoomCount, this);
+
+                                    RoomCount++;
+
+                                    r.EnMember(c);
+                                    c.mnumber = 0;
+                                    c.phase = 1;
+
+                                    guest.SendMsg("0#crte Agree");
+                                    rooms.Add(r);
+
+                                    
+                                    LogView(c.name + "님이" + content + "라는 새로운 방을 만들었습니다.");
+
+                                }
+                                //방 참여 신호를 받았을 때
+                                else if (tag == "#join ")
+                                {
+                                    int target = Convert.ToInt32(content);
+
+                                    bool jud = false;
+                                    foreach(Room r in rooms)
+                                    {
+                                        //들어갈 방 확인
+                                        if(r.number == target)
+                                        {
+                                            int n = r.EnMember(c);
+                                            c.guest.SendMsg("0#join " + n.ToString());
+                                            if(n != 99) c.phase = 1;
+                                            jud = true;
+                                        }
+                                    }
+                                    if (!jud)
+                                        guest.SendMsg("0#join 99");
+
+                                }
+                                //전체 채팅 신호를 받았을 때
+                                else if (tag == "#allc ")
+                                {
+                                    Broadcast("0#allc " + c.name + " : " + content);
+                                    
+                                }
+
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        LogView(c.name + "님이 종료하셨습니다.");
+                        c.close();
+                        t.Dispose();
+                    }
+                }
+
+                c.tmrReceiver.Tick += Receive0;
+                c.tmrReceiver.Start();
+                clients.Add(c);
+
+            }
+        }
+    
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            FormClosing += MainForm_FormClosing;
+            btnSend.Click += BtnSend_Click;
+
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            Broadcast("0#allc 공지 : " + txtSend.Text);
+            Broadcast("1#locc 공지 : " + txtSend.Text);
+
+        }
+
+        //로그
+        public void LogView(string txt)
+        {
+            txtLog.AppendText(txt + "\r\n");
+            txtLog.Focus();
+            txtLog.ScrollToCaret();
+
+            btnClient.Text = "클라 수 : " + (clients.Count).ToString() ;
+            btnRoom.Text = "방 개수 : " + (rooms.Count).ToString();
+
+        }
+
+        //폼 닫기
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            listener.Close();
+
+
+            foreach(Client client in clients)
+            {
+                client.close();
+
+            }
+
+        }
+        //모든 멤버에게 보내기
+        public void Broadcast(string txt)
+        {
+            foreach(Client client in clients)
+            {
+                client.guest.SendMsg(txt);
+            }
+
+        }
+    }
+}
